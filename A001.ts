604c514bf2a6c8f11d41702049642e68cfcc57f1
kaizy1227 <48644715+kaizy1227@users.noreply.github.com>
2020-09-26 16:43:19 +0700

    Add files via upload

---
+++ A001.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>A001</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>cc339ed3-dcc5-4929-b71e-e01d4fec7047</testSuiteGuid>
+   <testCaseLink>
+      <guid>206192dd-fc85-4533-8258-8c28f79aac87</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_001</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>693a51bf-94f0-45f1-a703-71ff8145552c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_002</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>486d1c12-fe83-4f60-8c29-a92c44deb727</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_003</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e2a5a52b-ce03-4d22-882b-707121763866</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_004</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>59114f1c-4af8-4b60-8e3b-c14699a2a7b0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_005</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
